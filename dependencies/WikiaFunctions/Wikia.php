d6dbc49cf0e896d6fde19bcc8ad9e995bb284ce4
Ty <tyler@faceyspacies.com>
2014-12-31 23:25:54 -0600

    Oops

--- dependencies/WikiaFunctions/Wikia.php
+++ dependencies/WikiaFunctions/Wikia.php
@@ -16,4 +16,59 @@ class Wikia {
 		}
 		return $headers;
 	}
+	
+	/**
+     * successbox
+     *
+     * @access public
+     * @static
+     * @author eloy@wikia
+     *
+     * @param string $what message for user
+     *
+     * @return string composed HTML/XML code
+     */
+    static public function successbox($what) {
+        return Xml::element( "div", array(
+				"class"=> "successbox", "style" => "margin: 0;margin-bottom: 1em;"
+			), $what)
+			. Xml::element("br", array( "style" => "clear: both;"));
+    }
+    /**
+     * errorbox
+     *
+     * return div with error message
+     *
+     * @access public
+     * @static
+     * @author eloy@wikia
+     *
+     * @param string $what message for user
+     *
+     * @return string composed HTML/XML code
+     */
+    static public function errorbox($what) {
+        return Xml::element( "div", array(
+				"class"=> "errorbox", "style" => "margin: 0;margin-bottom: 1em;"
+			), $what )
+			. Xml::element("br", array( "style" => "clear: both;"));
+    }
+    /**
+     * errormsg
+     *
+     * return span for error message
+     *
+     * @access public
+     * @static
+     * @author eloy@wikia
+     *
+     * @param string $what message for user
+     *
+     * @return string composed HTML/XML code
+     */
+    static public function errormsg($what)
+    {
+        return Xml::element("span", array( "style"=> "color: #fe0000; font-weight: bold;"), $what);
+    }
+}
 }
